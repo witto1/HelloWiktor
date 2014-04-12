baa5cbdcdb0249f373325b4c7577830e76e77d84
Wiktor Jungowskian <wiktorjun@yahoo.com>
2014-04-12 12:19:03 +0200

    added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWiktor::Application.config.secret_key_base = '495aa0b861b5f22bd13966434f4636cb913f6833924cd732852c3dc17880f10d092fa8d95f2d75559cd7513e1d0745331bc257f6117cc5226fa63759103d8247'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelloWiktor::Application.config.session_store :cookie_store, key: '_HelloWiktor_session'
